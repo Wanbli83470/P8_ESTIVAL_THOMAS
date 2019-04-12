c210416c470ddbeaf013eaff5825aa2d00bf3bf4
thomas83470 <estival.t@hotmail.com>
2019-04-12 19:15:20 +0200

    Héritage et block content/title OK.

--- Pur_Beurre/P8/views.py
+++ Pur_Beurre/P8/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def Legal_notice(request):
-    return render(request, 'P8/base.html')
+    return render(request, 'P8/Legal_Notice.html')
 
 # Create your views here.
