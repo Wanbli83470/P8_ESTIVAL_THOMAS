20f61c5b985c4981bbf1cbe3d705fed8ad236c22
thomas83470 <estival.t@hotmail.com>
2019-04-12 19:21:01 +0200

    Ajout du framework bootstrap OK

--- Pur_Beurre/P8/views.py
+++ Pur_Beurre/P8/views.py
@@ -1,7 +1,10 @@
+""" imporation de render afin d'afficher le code HTML """
 from django.shortcuts import render
-from django.http import HttpResponse
 
+""" Création de la vue pour les mentions légales """
 def Legal_notice(request):
     return render(request, 'P8/Legal_Notice.html')
+
+""" Création de la vue pour les mentions légales """
 
 # Create your views here.
