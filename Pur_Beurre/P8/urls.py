72a3b92760bf10baa7e05eaa4a78e6c280a90629
thomas83470 <estival.t@hotmail.com>
2019-04-12 18:59:07 +0200

    Premières url et vue associée

---
+++ Pur_Beurre/P8/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from django.urls import include
+
+from . import views
+
+urlpatterns = [
+    path('Legal_notice',views.Legal_notice),
+]
--- Pur_Beurre/P8/views.py
+++ Pur_Beurre/P8/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def Legal_notice(request):
+    return HttpResponse("Hello World !!!")
 
 # Create your views here.
