8e7a42a95f9791cef250bdcbf9e4a3f1cfccb79c
thomas83470 <estival.t@hotmail.com>
2019-05-01 16:50:31 +0200

    Premières table BDD

--- Pur_Beurre/P8/admin.py
+++ Pur_Beurre/P8/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, PRODUIT, CATEGORIES, SUBSTITUT
 # Register your models here.
 admin.site.register(Person)
+
+admin.site.register(PRODUIT)
+
+admin.site.register(CATEGORIES)
+
+admin.site.register(SUBSTITUT)
