02ed94e5efe572ad00b0a247d82cdc37a68aadd2
thomas83470 <estival.t@hotmail.com>
2019-05-07 09:03:24 +0200

    POO scrapping

--- Pur_Beurre/P8/food_scrap.py
+++ Pur_Beurre/P8/food_scrap.py
@@ -2,9 +2,69 @@ import requests as r
 from bs4 import BeautifulSoup as b
 import unicodedata
 
-class get_data :
-    def __init__(self):
-        pass
+class Scrapping_json :
+    def __init__(self, product):
+        self.product = product
+
+    def get_product_url(self, link_product=""):
+        self.link_product = link_product
+        requête = r.get("https://fr.openfoodfacts.org/cgi/search.pl?search_terms={}&search_simple=1&action=process".format(self.product))
+        html = requête.content
+        soup = b(html, 'html.parser')
+        list_products = soup.select(".products")[0]
+        product_one = list_products.li
+
+        # On récupère une url
+        nb = 0
+
+        for link in list_products.find_all('a'):
+            while nb < 1:
+                self.link_product = link.get('href')
+                # print(link_product)
+                nb += 1
+
+        return self.link_product
+
+
+    def get_categorie_url(self, link_cat = ""):
+        self.link_cat = link_cat
+
+        print("on rentre dans get_categorie_url")
+
+        link_product_complete = "https://fr.openfoodfacts.org{}".format(self.link_product)
+        print(link_product_complete)
+        cat_requête = r.get(link_product_complete)
+        cat_html = cat_requête.content
+        cat_soup = b(cat_html, 'html.parser')
+        self.link_cat = cat_soup.select(".tag.well_known")[2]
+        self.link_cat = self.link_cat.text
+
+        print("lien catégorie : {}".format(self.link_cat))
+        return self.link_cat
+
+
+    def get_json_categorie(self, link_cat = ""):
+        self.link_cat = link_cat
+        print("\nOn rentre dans get_json_categorie \n")
+        # Traitement de link_cat en vue de l'url
+
+        # Suppresion des caracètre accentués
+        self.link_cat = self.link_cat.lower()
+        # remplacement des espaces par un tiret
+        self.link_cat = self.link_cat.replace(" ", "-")
+
+        # Suppresion des accents
+        self.link_cat = unicodedata.normalize('NFKD', self.link_cat).encode('ascii', 'ignore')
+        self.link_cat = self.link_cat.decode('utf-8')
+
+        url_cat_json = "https://fr-en.openfoodfacts.org/category/{}.json".format(self.link_cat)
+
+        return url_cat_json
+
+
+class Api_get_data :
+    def __init__(self, nb_product=100):
+        self.nb_product = nb_product
 
 def get_category(choice_product) :
     # On choisit un produit
@@ -34,9 +94,9 @@ def get_category(choice_product) :
 
 
     link_product_complete = "https://fr.openfoodfacts.org{}".format(link_product)
-    # print(link_product_complete)
+    print(link_product_complete)
 
-    """Etape 2 : Récupérer le nom de la catégorie."""
+    """Etape 2 : Récupérer le liens de la catégorie."""
 
     cat_requête = r.get(link_product_complete)
     cat_html = cat_requête.content
@@ -64,4 +124,16 @@ def get_category(choice_product) :
     # print(url_cat_json)
     return url_cat_json
 
-test = get_category('chips')
+
+
+
+# On initialise l'instance de classe
+Nutella = Scrapping_json("Nutella")
+
+Nutella.get_product_url()
+
+MA_CATÉGORIE = Nutella.get_categorie_url()
+
+JSON = Nutella.get_json_categorie(link_cat=MA_CATÉGORIE)
+print(JSON)
+
