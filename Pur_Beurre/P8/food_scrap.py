bb4c742b3a5ed01ae635dbb58598aa071765ed68
thomas83470 <estival.t@hotmail.com>
2019-05-08 09:15:11 +0200

    requête API/simplification du scrapping

--- Pur_Beurre/P8/food_scrap.py
+++ Pur_Beurre/P8/food_scrap.py
@@ -1,6 +1,8 @@
 import requests as r
 from bs4 import BeautifulSoup as b
 import unicodedata
+import json
+
 
 class Scrapping_json :
     def __init__(self, product):
@@ -62,78 +64,21 @@ class Scrapping_json :
         return url_cat_json
 
 
-class Api_get_data :
-    def __init__(self, nb_product=100):
-        self.nb_product = nb_product
 
-def get_category(choice_product) :
-    # On choisit un produit
-    product = choice_product
-    # On paramètre la requête avec le produit pour obtenir le html correspondant
-    requête = r.get("https://fr.openfoodfacts.org/cgi/search.pl?search_terms={}&search_simple=1&action=process".format(product))
+# # On initialise l'instance de classe
+# Nutella = Scrapping_json("Nutella")
+#
+# Nutella.get_product_url()
+#
+# MA_CATÉGORIE = Nutella.get_categorie_url()
+#
+# JSON = Nutella.get_json_categorie(link_cat=MA_CATÉGORIE)
+# print(JSON)
 
-    # On récupère le contenu du code html
-    html = requête.content
+category = r.get('https://fr.openfoodfacts.org/api/v0/produit/3564700007341.json')
+print(category)
 
-    # On récupère proprement le contenu
-    soup = b(html, 'html.parser')
-
-    # On récupère la liste des produits
-    list_products = soup.select(".products")[0]
-    product_one = list_products.li
-
-    # On récupère une url
-    nb = 0
-
-    link_product = str
-    for link in list_products.find_all('a'):
-        while nb < 1:
-            link_product = link.get('href')
-            # print(link_product)
-            nb += 1
-
-
-    link_product_complete = "https://fr.openfoodfacts.org{}".format(link_product)
-    print(link_product_complete)
-
-    """Etape 2 : Récupérer le liens de la catégorie."""
-
-    cat_requête = r.get(link_product_complete)
-    cat_html = cat_requête.content
-    cat_soup = b(cat_html, 'html.parser')
-
-    link_cat = cat_soup.select(".tag.well_known")[2]
-    link_cat = link_cat.text
-    # print("lien catégorie : {}".format(link_cat))
-
-    """Etape 3 : parcourir le json de la catégorie"""
-
-    # Traitement de link_cat en vue de l'url
-
-    # Suppresion des caracètre accentués
-    link_cat = link_cat.lower()
-
-    # remplacement des espaces par un tiret
-    link_cat = link_cat.replace(" ", "-")
-
-    # Suppresion des accents
-    link_cat = unicodedata.normalize('NFKD', link_cat).encode('ascii', 'ignore')
-    link_cat = link_cat.decode('utf-8')
-    # print(link_cat)
-    url_cat_json = "https://fr-en.openfoodfacts.org/category/{}.json".format(link_cat)
-    # print(url_cat_json)
-    return url_cat_json
-
-
-
-
-# On initialise l'instance de classe
-Nutella = Scrapping_json("Nutella")
-
-Nutella.get_product_url()
-
-MA_CATÉGORIE = Nutella.get_categorie_url()
-
-JSON = Nutella.get_json_categorie(link_cat=MA_CATÉGORIE)
-print(JSON)
-
+category_json = category.json()
+category_json = category_json['product']['categories_tags'][0]
+category_json = category_json[3:]
+print(category_json)
