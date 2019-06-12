8e2429973dbdea87151061c2af615d9c94d6e2f9
thomas83470 <estival.t@hotmail.com>
2019-06-12 07:37:12 +0200

    correction wsgi

--- Pur_Beurre/Pur_Beurre/wsgi.py
+++ Pur_Beurre/Pur_Beurre/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pur_Beurre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pur_Beurre.Pur_Beurre.settings')
 
 application = get_wsgi_application()
