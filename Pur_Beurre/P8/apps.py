9459211ee69048776bacf5ba3c3a466df90d49b9
thomas83470 <estival.t@hotmail.com>
2019-06-12 09:57:13 +0200

    changement structure 2

--- Pur_Beurre/P8/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class P8Config(AppConfig):
-    name = 'P8'
--- Pur_Beurre/P8/context_processors.py
+++
@@ -1,3 +0,0 @@
-def ma_var(a):
-    ma_var = "Une chaine avec le context-processor"
-    return {'ma_var':ma_var}
