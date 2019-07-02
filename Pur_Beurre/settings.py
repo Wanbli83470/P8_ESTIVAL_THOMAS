dfb83c4a324db6380eab54bcf872628980e2b0b0
thomas83470 <estival.t@hotmail.com>
2019-07-02 09:49:19 +0200

    Avancée déploiement heroku

--- Pur_Beurre/settings.py
+++ Pur_Beurre/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@!!xzi7))avtfcxk44g4^xsoad*c++ex-z2em#6dxpsqywlv-='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['wanblipurbeurre.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'wanblipurbeurre.herokuapp.com']
 
 
 # Application definition
@@ -87,8 +87,8 @@ DATABASES = {
         'PORT': '',
     }
 }
-
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
