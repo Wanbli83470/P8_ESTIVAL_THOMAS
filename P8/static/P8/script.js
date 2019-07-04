c4bc96e60b1eecc946900eabd27a651df5470660
thomas83470 <estival.t@hotmail.com>
2019-07-04 08:38:33 +0200

    Amélioration responsive

--- P8/static/P8/script.js
+++ P8/static/P8/script.js
@@ -2,6 +2,29 @@ $(document).ready(function(){
     $("#footer").click(function(){
         alert("La detection Jquery fonctionne");
     });
+
+    $("#icône_accueil").mouseover(function(){
+        $("#icône_accueil").addClass("mr-3");
+    });
+
+    $("#icône_accueil").mouseleave(function(){
+        $("#icône_accueil").removeClass("mr-3");
+    });
+
+//    Responsive
+var width = document.body.clientWidth;
+console.log(width)
+
+if (width < 500) {
+console.log("plus petit que 1000");
+$("#img_icône_accueil").css("width", "50%");
+$("#ns").removeClass("w-25");
+$("#ns").addClass("w-50");
+$("#food_details").removeClass("w-50");
+$("#food_details").addClass("w-100");
+}
+else {
+console.log("plus grand que 1000")}
 });
 
 
