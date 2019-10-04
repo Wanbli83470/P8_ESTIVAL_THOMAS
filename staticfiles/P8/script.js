f2607241a600844276c76ea6c5d3a5b38cb32fb0
thomas83470 <estival.t@hotmail.com>
2019-10-04 21:38:45 +0200

    git commit -am "vue soutenance"

---
+++ staticfiles/P8/script.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+
+    $(".button_save").click(function(){
+        $(this).addClass("disabled");
+        $(this).html("<strong> déjà sauvegardé </strong>");
+
+    });
+
+    $("#icône_accueil").mouseover(function(){
+        $("#icône_accueil").addClass("mr-3");
+    });
+
+    $("#icône_accueil").mouseleave(function(){
+        $("#icône_accueil").removeClass("mr-3");
+    });
+
+//    Responsive
+var width = document.body.clientWidth;
+console.log(width)
+
+if (width < 500) {
+console.log("plus petit que 1000");
+$("#img_icône_accueil").css("width", "50%");
+$("#ns").removeClass("w-25");
+$("#ns").addClass("w-50");
+$("#food_details").removeClass("w-50");
+$("#food_details").addClass("w-100");
+
+$(".btn").removeClass("w-25");
+$(".btn").addClass("w-50");
+
+}
+
+else {
+console.log("plus grand que 1000");
+}
+
+//Méthode ajax
+var addAjax = $(".add-ajax");
+var nb_products = $("#nb_products").text();
+
+addAjax.click(function(event){
+$("#ns").addClass("w-50");
+        nb_products = parseInt(nb_products)
+        nb_products += 1
+        $("#nb_products").text(nb_products);
+
+        event.preventDefault();
+            console.log("Form is not sending");
+            thisForm = $(this)
+            var actionEndpoint = thisForm.attr("action");
+            var httpMethod = thisForm.attr("method");
+            var formData = thisForm.serialize();
+
+
+            $.ajax({
+                url: actionEndpoint,
+                method: httpMethod,
+                data: formData,
+
+                    success:function(data){
+                        console.log("success")
+                        console.log(data)
+                    },
+                    error:function(errorData){
+                        console.log("error")
+                        console.log(errorData)
+                    }
+                })
+        })
+
+});
+
+
+
+
