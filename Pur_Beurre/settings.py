d08f919a82a9dfa8d9063274e7b8df9be23d636b
thomas83470 <estival.t@hotmail.com>
2019-10-04 21:45:15 +0200

    résolution css

--- Pur_Beurre/settings.py
+++ Pur_Beurre/settings.py
@@ -127,3 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
