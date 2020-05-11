9824a26b445aeb1ed84a0d4101eb97414ff6e85a
Wanbli83470 <estival.t@hotmail.com>
2020-05-11 09:18:15 +0200

    Travail pep8 update.py3

--- P8/update.py
+++ P8/update.py
@@ -16,7 +16,15 @@ PASSWORD = "jpmfmaemp73%"
 DATABASE = "pur_beurre"
 
 DATE_NOW = datetime.datetime.now()
-DICT_DATE = {1:"Janvier", 2:"Février", 3: "Mars", 4: "Avril", 5: "Mai", 6: "Juin", 7:"Juillet", 8:"Août", 9:"Septembre", 10:"Octobre", 11:"Novembre", 12:"Décembre"}
+
+DICT_DATE = {
+    1: "Janvier", 2: "Février",
+    3: "Mars", 4: "Avril",
+    5: "Mai", 6: "Juin",
+    7: "Juillet", 8: "Août",
+    9: "Septembre", 10: "Octobre",
+    11: "Novembre", 12: "Décembre"
+}
 
 try:
     # Connect to an existing database
